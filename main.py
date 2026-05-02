45d2b915da92a991738ba370b72c5395679d8525
RanaUniverse <RanaUniverse321@gmail.com>
2026-05-02 17:04:00 +0530

    now i added team and hero creation code in my main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from database_code.db_make import (
     engine,
     # create_db_and_tables,
 )
-from database_code.models import HeroModel
+from database_code.models import HeroModel, TeamModel
 
 
 import fake_data
@@ -34,8 +34,28 @@ def create_hero():
         print(f"Hero has been inserted, {hero_obj}")
 
 
+def create_hero_and_team():
+    with Session(engine) as session:
+        team_obj = TeamModel(
+            name=fake_data.get_a_full_name(),
+            headquarters=fake_data.one_word(),
+        )
+        hero_obj = HeroModel(
+            name=fake_data.get_a_full_name(),
+            secret_name=fake_data.one_word(),
+            age=fake_data.age_int(),
+            phone="1234566741",
+        )
+        hero_obj.team = team_obj
+        session.add(hero_obj)
+        session.commit()
+        session.refresh(hero_obj)
+        print("Hero details are", hero_obj)
+
+
 if __name__ == "__main__":
     # create_db_and_tables()
     how_many = 3
     for _ in range(how_many):
-        create_hero()
+        # create_hero()
+        create_hero_and_team()
